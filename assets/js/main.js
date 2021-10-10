b57a348d750930786983082997927103213fa2a5
Galitbenzadok <galitbenzadok39@gmail.com>
2021-10-11 00:51:10 +0300

    delete button added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,11 @@
 let tasks = []
 
+let id = 0
+
 function saveTask() {
     const form = document.querySelector('form')
-    let task = {}
-
+    let task = {} 
+    const taskId = id
     const taskContent = form.elements['task'].value
     const dateOfTask = form.elements['date'].value
     const timeOfTask = form.elements['time'].value
@@ -11,58 +13,106 @@ function saveTask() {
     task.taskContent = taskContent
     task.dateOfTask = dateOfTask
     task.timeOfTask = timeOfTask
+    task.taskId = id
+    id++
 
+    // tasks = getArrayFromLocalStorage()
     tasks.push(task)
-    randerTasks(tasks)
+    // const tasksStringify = JSON.stringify(tasks)
+    // localStorage.setItem('tasks', tasksStringify)  
+    renderTasks()     
 }
+
+
+// function getArrayFromLocalStorage() {
+//     const arrayStringify = localStorage.getItem('tasks')
+//     let array
+//     if (arrayStringify) {
+//         array = JSON.parse(arrayStringify)
+//     }
+//     else {
+//         array = []
+//     }
+//     return array
+// }
+
 
 function creatTask(text){
     const mainDiv = document.createElement('div')
+    mainDiv.setAttribute('id', text.taskId)
     mainDiv.classList.add ('innerTask')
+    const id = text.taskId
 
     const taskContentDiv = document.createElement('div')
     taskContentDiv.innerHTML = text.taskContent
     taskContentDiv.classList.add ('taskText')   
+    mainDiv.appendChild(taskContentDiv)
     
     const dateOfTaskDiv = document.createElement('div')
     dateOfTaskDiv.innerHTML = text.dateOfTask
     dateOfTaskDiv.classList.add ('smallerFont')
-
+    mainDiv.appendChild(dateOfTaskDiv)
 
     const timeOfTaskDiv = document.createElement('div')
     timeOfTaskDiv.innerHTML = text.timeOfTask
     timeOfTaskDiv.classList.add ('smallerFont')
+    mainDiv.appendChild(timeOfTaskDiv)  
+    
+    const deleteDiv = document.createElement('div') 
+    deleteDiv.innerHTML = "<button class='deleteBut hidden' id='but" + id + "' onclick='deleteTask(" + id + ")'>X</button>"
+    mainDiv.appendChild(deleteDiv)  
 
-    mainDiv.appendChild(taskContentDiv)
-    mainDiv.appendChild(dateOfTaskDiv)
-    mainDiv.appendChild(timeOfTaskDiv)
+    mainDiv.addEventListener("mouseover", function() {showDeleteBut(id)});
+    mainDiv.addEventListener("mouseout", function() {hideDeleteBut(id)});
 
     return mainDiv
 }
 
 function creatCard(){
     const taskContentDiv = document.createElement('div')
-    const deleteDiv = document.createElement('div')    
-    let task
-    let deleteBut
-
-    for (let i in tasks){
-        task = creatTask(tasks[i])  
-        deleteDiv.innerHTML = "<button class='deleteBut' onclick = 'ShowOrHideDeleteBut()'>X</button>"
-        // deleteDiv.click = ShowOrHideDeleteBut()
-    }
-    taskContentDiv.appendChild(task)
-    taskContentDiv.appendChild(deleteDiv)
     taskContentDiv.classList.add('card')
+    const deleteDiv = document.createElement('div') 
+    let taskFill
+    let butId
+   
+    for (let i=0; i<tasks.length; i++){
+        taskFill = creatTask(tasks[i]) 
+        butId = i       
+    }
+
+    taskContentDiv.appendChild(taskFill)  
+    taskContentDiv.appendChild(deleteDiv)  
+    taskContentDiv.setAttribute('id', 'div' + butId)
     return taskContentDiv
 }
 
-// function ShowOrHideDeleteBut(){
-//     const butDel = document.querySelector('.deleteBut')
-//     butDel.classList.add('hidden')    
-// }
+function deleteTask(numToDelete){   
+    const nTasks = tasks.filter(
+        function (obj){
+            if (obj.taskId != numToDelete){               
+                return true
+            }
+            return false
+        }
+    )
+tasks = nTasks
+const deleletCard = document.getElementById('div'+ numToDelete)
+deleletCard.remove()
+}
+  
+ 
+function showDeleteBut(num){
+    const but = document.getElementById('but'+ num)
+    but.classList.remove('hidden')
+}
 
-function randerTasks(){
+function hideDeleteBut(num){
+    const but = document.getElementById('but'+ num)
+    but.classList.add('hidden')
+ }
+
+function renderTasks(){
+    // tasks = getArrayFromLocalStorage()
     const div = document.querySelector('.cards')    
     const cards  = creatCard(tasks)
     div.appendChild(cards)
@@ -72,3 +122,10 @@ function deleteForm(){
     const form = document.querySelector('form')
     form.reset()
 }
+
+// renderTasks() 
+
+
+
+
+
